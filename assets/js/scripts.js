ddbeaefb02dbb2c4cf7046924094a890556bde84
Sebastián Muñoz <smc.archivera@gmail.com>
2026-06-23 22:28:03 -0400

    Arreglos en Smooth scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@
  * Componentes de Bootstrap JS utilizados:
  * - Carousel (v5.3.8) - Control nativo de diapositivas
  * - Tooltips (v5.3.8) - Inicializados mediante Popper.js y Bootstrap JS
+ * - Smooth scroll (Bootstrap CSS) - Desplazamiento suave
  */
 // Inicializa los tooltips
 console.log("Script cargado");
@@ -11,4 +12,20 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 
 tooltipTriggerList.forEach((tooltipTriggerEl) => {
     new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
 });
