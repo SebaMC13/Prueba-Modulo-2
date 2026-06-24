164c5f2185ffd4013a15727c596c1c719acb850e
Sebastián Muñoz <smc.archivera@gmail.com>
2026-06-23 22:48:54 -0400

    ultimo cambio, scroll-behavior de forma nativa en css (mucho mejor que codigo directo en js)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,6 @@
  * Componentes de Bootstrap JS utilizados:
  * - Carousel (v5.3.8) - Control nativo de diapositivas
  * - Tooltips (v5.3.8) - Inicializados mediante Popper.js y Bootstrap JS
- * - Smooth scroll (Bootstrap CSS) - Desplazamiento suave
  */
 // Inicializa los tooltips
 console.log("Script cargado");
@@ -12,20 +11,4 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 
 tooltipTriggerList.forEach((tooltipTriggerEl) => {
     new bootstrap.Tooltip(tooltipTriggerEl);
-});
-
-
-$(document).ready(function () {
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                window.location.hash = hash;
-            });
-        }
-    });
 });
